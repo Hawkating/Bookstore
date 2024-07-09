bdfe3810637f2b5b6b7973b351b9280659ba9c04
Jan B. <j.bruchwalski@web.de>
2024-07-09 23:46:26 +0200

    finalized

--- js/script.js
+++ js/script.js
@@ -1,6 +1,86 @@
 function render(){
-let content = document.getElementById('content');
+    let initArray = localStorage.getItem("keyBooks");
+    if (initArray) {
+        books = load();
+      }
+
+   let content = document.getElementById('content');
 content.innerHTML = ``;
-content.innerHTML = returnStartContent();
-    
+content.innerHTML = returnHeader();
+let allCards = document.getElementById('all-cards');
+
+for (let i = 0; i < books.length; i++){
+    allCards.innerHTML += returnCards(i);
+    if (books[i]["liked"]){
+        document.getElementById(`likedpic${i}`).src = src="./img/likecolored.png"
+    }
+    renderComments(i);
+    if(books[i]["comments"].length < 1){
+        hideCommentArea(i);
+    }
+}
+}
+
+
+function renderComments(index){
+    let commentArea = document.getElementById(`comments${index}`);
+
+    for (let i = 0 ; i < books[index]["comments"].length; i++){
+    commentArea.innerHTML += `
+    <table>
+        <tr>
+            <td class="table-data">${books[index]["comments"][i]["name"]}</td><td class="table-data">:${books[index]["comments"][i]["comment"]}</td>
+        </tr>
+    </table>
+    `;
+}
+}
+
+
+function hideCommentArea(index){
+document.getElementById(`commentCard${index}`).classList.add('d-none');
+
+}
+
+
+function like(index){
+    books[index]['liked'] = !books[index]['liked'];
+    if (books[index]['liked']){
+        let likeNumber = books[index]["likes"];
+        likeNumber = likeNumber + 1;
+        books[index]["likes"] = likeNumber;
+}else {
+    let likeNumber = books[index]["likes"];
+        likeNumber = likeNumber - 1;
+        books[index]["likes"] = likeNumber;
+}
+    save();
+    render();
+}
+
+
+function sendComment(index){
+    let text = document.getElementById(`textarea${index}`).value;
+    if (text){
+    let commentArray = books[index]["comments"];
+    commentArray.push({"name": "TestACCOUNT", "comment": text})
+};  
+    save();
+    render();
+}
+
+
+function saveWithoutRender(){
+    let arrayAsText = JSON.stringify(books);
+    localStorage.setItem('keyBooks', arrayAsText);
+}
+
+
+function save(){
+    let arrayAsText = JSON.stringify(books);
+    localStorage.setItem('keyBooks', arrayAsText);
+}
+
+function load(){
+   return JSON.parse(localStorage.getItem("keyBooks"));
 }
--- js/templates.js
+++ js/templates.js
@@ -1,9 +1,42 @@
-function returnStartContent(){
-     return /*html*/ `
+function returnHeader() {
+    return /*html*/ `
         <header>
             <img class="headline-logo" src="./img/logo.png">
-            <h1><font>Jans</font>Buchladen - </h1><h2>get your book here</h2>
-            
+            <h1><font>Jans</font>Buchladen</h1><h2><span>-</span> get your book here</h2>
         </header>
-        ` 
+        <div id="all-cards"></div>
+        `
+};
+
+
+function returnCards(index) {
+    let priceNumber = books[index]["price"].toFixed(2);
+    let priceString = priceNumber.toString();
+    priceString = priceString.replace(".", ",");
+
+    return /*html*/ `
+    <div class="card">
+        <div class="border-bottom centered card-headline"><h2>${books[index]["name"]}</h2></div>
+        <div class="border-bottom centered pic"><img class="bookpic" src="./img/book.png"></div>
+        <div>
+            <table class="table-price-likes">
+                <tr>
+                    <td class="price">${priceString}€</td><td class="likes">${books[index]["likes"]}<img id="likedpic${index}" onclick="like(${index})" class="pic-heart" src="./img/like.png"></td>
+                </tr>
+            </table>
+        </div>
+        <div class="table-infos-div border-bottom">
+            <table class="table-infos">
+                <tr><td class="table-data">Autor</td><td>: ${books[index]["author"]}</td></tr>
+                <tr><td class="table-data">Erscheinungsjahr</td><td>: ${books[index]["publishedYear"]}</td></tr>
+                <tr><td class="table-data">Genre</td><td>: ${books[index]["genre"]}</td></tr>
+            </table>
+        </div>
+        <div id="commentCard${index}">
+            <div class="commentsHeadline">Kommentare:</div>
+            <div class="commentsArea" id="comments${index}"></div>
+        </div>
+        <div class="textareaBox"><textarea id="textarea${index}" class="textarea" placeholder="Schreibe dein Kommentar"></textarea><img class="send-button" onclick="sendComment(${index})" src="./img/send.png"></div>
+    </div>
+    `
 }
